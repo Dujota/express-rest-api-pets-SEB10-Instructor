36329974c93efb49507477186c1b6ccaae113d01
Denis S Dujota <denisdujota@gmail.com>
2024-08-11 12:44:59 +0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const cors = require('cors');
 
 // controllers
 const petsRouter = require('./controllers/pets.js');
@@ -9,6 +10,11 @@ dotenv.config();
 require('./config/database');
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 app.use(morgan('dev'));
 app.use(express.json());
 
